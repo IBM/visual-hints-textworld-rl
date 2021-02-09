45db5d916f0887136ff0b22196a31193ad84ed45
Michiaki Tatsubori <mich@jp.ibm.com>
2021-02-09 16:48:18 +0900

    Added a package setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tw_cooking_game_puzzle',
+    version='0.1.0',
+    packages=['tw_cooking_game_puzzle'],
+    install_requires=[
+            'pillow'
+    ],
+)
